f4ad10967becf4cd55a975b3ad93633b0496fa40
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-03 11:59:10 +0500

    Update variables.js

--- script/variables.js
+++ script/variables.js
@@ -1,1 +1,7 @@
+const cards = document.querySelector(".cards");
+const main = document.querySelector("main");
+const mainSwiper = document.querySelector(".swiper-wrapper");
+const search = document.querySelector(".search")
+const nav = document.querySelector("nav")
+
 
