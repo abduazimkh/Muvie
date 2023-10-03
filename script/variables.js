77185215c6884b1506945b27dc89b6a4d01ae4a4
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-03 11:59:28 +0500

    Update variables.js

--- script/variables.js
+++ script/variables.js
@@ -5,3 +5,9 @@ const search = document.querySelector(".search")
 const nav = document.querySelector("nav")
 
 
+const api = `https://api.themoviedb.org/3/movie/top_rated?&with_networks=213`;
+const first = `https://api.themoviedb.org/3/discover/tv`;
+const second = `https://api.themoviedb.org/3/discover/movie?&with_genres=28`;
+const third = `https://api.themoviedb.org/3/discover/movie?&with_genres=12`;
+const furth = `https://api.themoviedb.org/3/discover/movie?&with_genres=35`;
+const five = `https://api.themoviedb.org/3/discover/movie?&with_genres=16`;
